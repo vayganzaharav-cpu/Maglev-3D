da02b2c74e00f2c84a0d2b6aeaf3d66611d81f51
vayganzaharav-cpu <vayganzaharav@gmail.com>
2026-04-19 20:22:57 +0300

    Maglev 3D.py

--- Maglev 3D.py
+++ Maglev 3D.py
@@ -1,6 +1,4 @@
-Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> import streamlit as st
+    import streamlit as st
 ... import streamlit.components.v1 as components
 ... 
 ... # Настройка страницы
